1cb770ff3e301ff20f39d3b76dc300ef015649ab
Jeonyul <37632366+Jeonyul@users.noreply.github.com>
2018-04-27 19:05:47 +0900

    Add files via upload

--- HangmanGame/main.cpp
+++ HangmanGame/main.cpp
@@ -1,68 +1,77 @@
-#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <string>
+#include <iostream>
 #include <vector>
 using namespace std;
 /*
-	------
-	|    |
-	|    O
-	|   \|/
-	|    |
-	|   / \
-   / \
+------
+|    |
+|    O
+|   \|/
+|    |
+|   / \
+/ \
 */
 
 int main()
 {
-	vector<string> strings;
-	strings.push_back("programming");
-	strings.push_back("network");
-	strings.push_back("engineer");
-	string secretStr = strings[rand() % strings.size()];
-	string displayedStr(secretStr.length(), '-');
-	string guessedChars;
-	int life = 10;
-	cout << displayedStr << endl;
-	while (true)
-	{
-		cout << "Your chance: " << life << endl;
-		cout << "Guessed letter:" << guessedChars << endl;
-		
-		char input;
-		do
-		{
-			cout << "Guess a character: ";
-			cin >> input;
-		} while ((displayedStr.find(input) != displayedStr.npos) ||
-			(guessedChars.find(input) != guessedChars.npos));
+	srand((unsigned int)time(0));
+	vector<string> words;
+	words.push_back("thinkpad");
+	words.push_back("samsung");
+	words.push_back("amazon");
+	words.push_back("protein");
+	random_shuffle(words.begin(), words.end());
+	const string THE_WORD = words[0];
+	string soFar(THE_WORD.size(), '-');
+	string used;
+	constexpr int MAX_WRONG = 8;
+	unsigned int wrong = 0;
 
-		int indexFound = secretStr.find(input);
-		guessedChars = guessedChars + " " + input;
-		if (indexFound == secretStr.npos)
+	cout << "Welcome to Hangman. Good luck!\n\n\n";
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		system("CLS");
+		cout << "You have " << (MAX_WRONG - wrong) << " incorrect guesses left.\n\n";
+
+		cout << "You've used the following letters: " << used << "\n\n\n";
+		cout << "So far, the words is: \n" << soFar << "\n\n\n";
+		
+		// control user input
+		cout << "Enter your guess: ";
+		char guess;
+		cin >> guess;
+		while (used.find(guess) != used.npos)
 		{
-			life--;
+			cout << "Enter your guess: ";
+			cin >> guess;
 		}
-		else
+		cout << "\n\n\n";
+		used = used + guess + ", ";
+
+		// modify string soFar
+		int matchCount = 0;
+		int i = THE_WORD.find(guess);
+		while (i != THE_WORD.npos)
 		{
-			while (indexFound != secretStr.npos)
-			{
-				displayedStr[indexFound] = secretStr[indexFound];
-				indexFound = secretStr.find(input, indexFound + 1);
-			}
+			soFar[i] = guess;
+			matchCount++;
+			i = THE_WORD.find(guess, i + 1);
 		}
-		cout << displayedStr << endl;
-		if (displayedStr == secretStr)
+		if (matchCount == 0)
 		{
-			cout << "Congratulation!" << endl;
-			break;
-		}
-		else if (life == 0)
-		{
-			cout << "You hanged.." << endl;
-			cout << "The word was " << secretStr << "." << endl;
-			break;
+			wrong++;
 		}
 	}
-	
+	if (soFar == THE_WORD)
+	{
+		cout << "Congratulation! You guessed it, \"" << soFar << "\"!\n\n";
+	}
+	else
+	{
+		cout << "You've been hanged!\n\n" << endl;
+	}
 	return 0;
 }
